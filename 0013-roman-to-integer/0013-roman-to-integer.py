b363486f9bd563ce7ce3bfb8216a7b1510cf3f3e
Yash Lahase <yash.lahase@adypu.edu.in>
2026-02-04 12:11:45 +0530

    Time: 12 ms (37.84%), Space: 12.3 MB (89.1%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        value = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+
+        for i in range(len(s)):
+            # if next symbol is bigger, subtract current
+            if i + 1 < len(s) and value[s[i]] < value[s[i + 1]]:
+                total -= value[s[i]]
+            else:
+                total += value[s[i]]
+
+        return total
