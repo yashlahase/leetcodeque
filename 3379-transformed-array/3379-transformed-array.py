4d1adeb15a88127fa5fcd57b2a6745ba4f48b0cc
Yash Lahase <yash.lahase@adypu.edu.in>
2026-02-05 10:34:03 +0530

    Time: 56 ms (74.44%), Space: 19.4 MB (10.76%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+
+        for i in range(n):
+            if nums[i] == 0:
+                result[i] = 0
+            else:
+                new_index = (i + nums[i]) % n
+                result[i] = nums[new_index]
+
+        return result
